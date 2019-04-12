9f14ef6b89699c36d5e1870ec074c29876dea197
tgalvinjr <tgalvin.jr@gmail.com>
2019-04-12 04:52:53 +0300

    add images on carousel at index.html

--- js/orders.js
+++ js/orders.js
@@ -1,18 +1,30 @@
 var numberPax = "";
 var foodSelected = "";
 
-
-$(document).ready(function(){
-    $(".form").submit(function(event){
+//UI Logic
+$(document).ready(function () {
+    $(".form").submit(function (event) {
         event.preventDefault()
         numberPax = $("#number").val();
-        foodSelected = $("#inputGroupSelect04 option:selected").text();      
-        if(foodSelected === "Kenya Style Pork"){
-            ribs=250*numberPax;
-            salt=1*numberPax;
+        foodSelected = $("#inputGroupSelect04 option:selected").text();
+
+//Business Logic
+        if (foodSelected === "Kenya Style Pork") {
+            ribs = 250 * numberPax;
+            salt = 1 * numberPax;
         }
-        alert ("use " + ribs + " grams of pork ribs");
-        alert ("use " + salt + " pinch(es) of salt");
-        else if(foodSelected){}
+        alert("use " + ribs + " grams of pork ribs");
+        alert("use " + salt + " pinch(es) of salt");
+        
+        else if (foodSelected === "Juicy Kuku Choma") {
+            chickenbreast = 250 * numberPax;
+            salt = 1 * numberPax;
+        }
+        alert("use " + ribs + " grams of chickenbreast");
+        alert("use " + salt + " pinch(es) of salt");
+
+        else if (foodSelected === "Party Pizza") {
+            
+        }
     })
 })
